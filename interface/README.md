f0eb26f1b508c3091c3f4010c9161f0b2624bf3b
Victor Rodriguez <awtGerry@gmail.com>
2024-04-23 20:05:39 -0600

    Agregar archivos svelte
    agregue navbar dentro de App.svelte
    deje un ejemplo del svg en Navbar.svelte
    agregue un README a interface/ para saber como va la estrucutra del
    programa

---
+++ interface/README.md
@@ -0,0 +1,5 @@
+|- interface
+|-- app: pages will live here
+|-- components: reusable components will live here
+|-- hooks: custom hooks will live here
+|-- lib: utility functions will live here
--- interface/app/Navbar.svelte
+++ interface/app/Navbar.svelte
@@ -1,53 +1,50 @@
-
-   <script>
-    //   import newFileIcon from '../newFile.svg'
+<script>
+  //  import newFileIcon from '../newFile.svg'
 </script>
 
+<nav class="flex items-center justify-between">
+  <ul>
+    <li>
+      <a href="/#" target="_blank" title="Nuevo archivo">
+        <img src="/new_file.svg" class="w-4 h-4" alt="" />
+        <span>Nuevo archivo</span>
+      </a>
 
-  
+      <a href="/#">nueva carpeta</a>
 
-  <nav>
-    <ul>
-      <li>
-        <a href="/#"> Nuevos Archivos </a>
+      <a href="/#">Guardar</a>
 
-        <a href="/#">nueva carpeta</a>
+      <a href="/#">Vista previa</a>
 
-        <a href="/#">Guardar</a>
-
-        <a href="/#">Vista previa</a>
-
-        <a href="nosotros.svelte">Acerca de nosotros</a>
+      <a href="nosotros.svelte">Acerca de nosotros</a>
 
 
-      </li>
-    </ul>
+    </li>
+  </ul>
 
-   
-  </nav>
-  
-  <style>
-nav {
-  background-color: #094067;
-  padding: 2.5rem 2rem;
-  flex-direction: row;
-}
+ 
+</nav>
 
-nav ul {
-  display: flex;
-  list-style: none;
-  margin: 0;
-  padding: 0;
-  flex-direction: row;
-}
+<style>
+  nav {
+    background-color: #094067;
+    flex-direction: row;
+  }
 
-nav li {
-  margin: 0;
-}
+  nav ul {
+    display: flex;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    flex-direction: row;
+  }
 
-a {
-  color: aliceblue;
-  padding: 0.5rem 1rem;
-}
+  nav li {
+    margin: 0;
+  }
 
+  a {
+    color: aliceblue;
+    padding: 0.5rem 1rem;
+  }
 </style>
