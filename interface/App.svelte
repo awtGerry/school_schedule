7b5b940a8059f30f648e2773220642c49a1c952b
moye30 <115683322+moye30@users.noreply.github.com>
2024-04-25 11:04:11 -0600

    implementacion de la navbar

--- interface/App.svelte
+++ interface/App.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
   import Greet from './lib/Greet.svelte'
   import Navbar from './app/Navbar.svelte'
+  import LoginForm from './app/LoginForm.svelte';
 </script>
 
 <main>
   <Navbar />
 </main>
+
+ <form>
+   <LoginForm />
+ </form>
--- interface/app/Navbar.svelte
+++ interface/app/Navbar.svelte
@@ -2,23 +2,103 @@
   //  import newFileIcon from '../newFile.svg'
 </script>
 
-<nav class="flex items-center justify-between">
+<nav>
   <ul>
     <li>
-      <a href="/#" target="_blank" title="Nuevo archivo">
-        <img src="/new_file.svg" class="w-4 h-4" alt="" />
+      <div class="logo">
+      <a href="/#" target="_blank" title="logo">
+      <img src="/logo.png" class="" alt="" />  
+      </div>
+
+     <div class="navbar1">
+      <a href="/login.svelte" target="_blank" title="Nuevo archivo">
+        <img src="/new_file.svg" class="" alt="" />
         <span>Nuevo archivo</span>
       </a>
 
-      <a href="/#">nueva carpeta</a>
+       <a href="/#" target="_blank" title="Nueva carpeta">
+        <img src="/new_folder.svg" class="" alt="" />
+        <span>Nueva carpeta</span>
+      </a>
 
-      <a href="/#">Guardar</a>
+       <a href="/#" target="_blank" title="Guardar">
+        <img src="/save.svg" class="" alt="" />
+        <span>Guadar</span>
+      </a>
 
-      <a href="/#">Vista previa</a>
+      <a href="/#" target="_blank" title="preview">
+      <img src="/preview.svg" class="" alt="">
+      <span>Vista previa</span>
+      </a>
 
-      <a href="nosotros.svelte">Acerca de nosotros</a>
+     
+      <a href="/#" target="_blank" title="printer">
+      <img src="/printer.svg" class="" alt="">
+      <span>Imprimir</span>
+      </a>
 
+      </div>
 
+      <div class="navbar2">
+      <a href="/#" target="_blank" title="group">
+      <img src="/group.svg" class="" alt="">
+      <span>Grupos</span>
+      </a>
+
+       <a href="/#" target="_blank" title="materias">
+      <img src="/materias.svg" class="" alt="">
+      <span>Materias</span>
+      </a>
+
+       <a href="/#" target="_blank" title="maestros">
+      <img src="/maestro.svg" class="" alt="">
+      <span>Maestros</span>
+      </a>
+
+       <a href="/#" target="_blank" title="aulas">
+      <img src="/aulas.svg" class="" alt="">
+      <span>Aulas</span>
+      </a>
+
+      </div>
+
+      <div>
+       <a href="/#" target="_blank" title="IA">
+      <img src="/ia.svg" class="" alt="">
+      <span>IA</span>
+      </a>
+       <a href="/#" target="_blank" title="hv">
+      <img src="/hv.svg" class="" alt="">
+      <span>Historial de versiones</span>
+      </a>
+
+      </div>
+
+      <div>
+       <a href="/#" target="_blank" title="generador">
+      <img src="/generador.svg" class="" alt="">
+      <span>Generar horario</span>
+      </a>
+
+      </div>
+
+      <div class="navbar3">
+       <a href="/#" target="_blank" title="school">
+      <img src="/school.svg" class="" alt="">
+      <span>Escuela/ Institucion</span>
+      </a>
+
+       <a href="/#" target="_blank" title="nosotros">
+      <img src="/nosotros.svg" class="" alt="">
+      <span>Sobre nosotros</span>
+      </a>
+
+       <a href="/#" target="_blank" title="ayuda">
+      <img src="/ayuda.svg" class="" alt="">
+      <span>Ayuda</span>
+      </a>
+      </div>
+    
     </li>
   </ul>
 
@@ -26,9 +106,15 @@
 </nav>
 
 <style>
-  nav {
+
+ 
+ nav {
     background-color: #094067;
     flex-direction: row;
+  }
+
+  .logo{
+    border-radius: 50%;
   }
 
   nav ul {
@@ -39,12 +125,21 @@
     flex-direction: row;
   }
 
-  nav li {
-    margin: 0;
-  }
+ 
 
   a {
+    font-size: 10px;
     color: aliceblue;
-    padding: 0.5rem 1rem;
+    padding-left: 0.5rem;
+    padding: 0.2rem 0.2rem;
+    display:inline-flex;
+    flex-direction:column; /* Apila los elementos verticalmente */
+    align-items:center; /* Centra los elementos horizontalmente */
+
+
   }
+
+
+ 
+
 </style>
