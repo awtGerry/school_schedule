9f44f635b04941274afbb83615f6a53df666e4ab
moye30 <115683322+moye30@users.noreply.github.com>
2024-04-25 20:42:41 -0600

    navbar casi terminada

--- interface/app/Navbar.svelte
+++ interface/app/Navbar.svelte
@@ -3,143 +3,140 @@
 </script>
 
 <nav>
-  <ul>
-    <li>
-      <div class="logo">
-      <a href="/#" target="_blank" title="logo">
-      <img src="/logo.png" class="" alt="" />  
-      </div>
+  <div class="navbar1">
+    <a href="/login.svelte" target="_blank" title="Nuevo archivo">
+      <img src="/new_file.svg" class="" alt="" />
+      <span>Nuevo archivo</span>
+    </a>
 
-     <div class="navbar1">
-      <a href="/login.svelte" target="_blank" title="Nuevo archivo">
-        <img src="/new_file.svg" class="" alt="" />
-        <span>Nuevo archivo</span>
-      </a>
+    <a href="/#" target="_blank" title="Nueva carpeta">
+      <img src="/new_folder.svg" class="" alt="" />
+      <span>Nueva carpeta</span>
+    </a>
 
-       <a href="/#" target="_blank" title="Nueva carpeta">
-        <img src="/new_folder.svg" class="" alt="" />
-        <span>Nueva carpeta</span>
-      </a>
+    <a href="/#" target="_blank" title="Guardar">
+      <img src="/save.svg" class="" alt="" />
+      <span>Guadar</span>
+    </a>
 
-       <a href="/#" target="_blank" title="Guardar">
-        <img src="/save.svg" class="" alt="" />
-        <span>Guadar</span>
-      </a>
-
-      <a href="/#" target="_blank" title="preview">
-      <img src="/preview.svg" class="" alt="">
+    <a href="/#" target="_blank" title="preview">
+      <img src="/preview.svg" class="" alt="" />
       <span>Vista previa</span>
-      </a>
+    </a>
 
-     
-      <a href="/#" target="_blank" title="printer">
-      <img src="/printer.svg" class="" alt="">
+    <a class="" href="/#" target="_blank" title="printer">
+      <img src="/printer.svg" class="" alt="" />
       <span>Imprimir</span>
-      </a>
+    </a>
 
-      </div>
-
-      <div class="navbar2">
-      <a href="/#" target="_blank" title="group">
-      <img src="/group.svg" class="" alt="">
+    <a href="/#" target="_blank" title="group">
+      <img src="/group.svg" class="" alt="" />
       <span>Grupos</span>
-      </a>
+    </a>
 
-       <a href="/#" target="_blank" title="materias">
-      <img src="/materias.svg" class="" alt="">
+    <a href="/#" target="_blank" title="materias">
+      <img src="/materias.svg" class="" alt="" />
       <span>Materias</span>
-      </a>
+    </a>
 
-       <a href="/#" target="_blank" title="maestros">
-      <img src="/maestro.svg" class="" alt="">
+    <a href="/#" target="_blank" title="maestros">
+      <img src="/maestro.svg" class="" alt="" />
       <span>Maestros</span>
-      </a>
+    </a>
 
-       <a href="/#" target="_blank" title="aulas">
-      <img src="/aulas.svg" class="" alt="">
+    <a href="/#" target="_blank" title="aulas">
+      <img src="/aulas.svg" class="" alt="" />
       <span>Aulas</span>
-      </a>
+    </a>
 
-      </div>
-
-      <div>
-       <a href="/#" target="_blank" title="IA">
-      <img src="/ia.svg" class="" alt="">
+    <a href="/#" target="_blank" title="IA">
+      <img src="/ia.svg" class="" alt="" />
       <span>IA</span>
-      </a>
-       <a href="/#" target="_blank" title="hv">
-      <img src="/hv.svg" class="" alt="">
+    </a>
+
+    <a href="/#" target="_blank" title="hv">
+      <img src="/hv.svg" class="" alt="" />
       <span>Historial de versiones</span>
-      </a>
+    </a>
 
-      </div>
-
-      <div>
-       <a href="/#" target="_blank" title="generador">
-      <img src="/generador.svg" class="" alt="">
+    <a href="/#" target="_blank" title="generador">
+      <img src="/generador.svg" class="" alt="" />
       <span>Generar horario</span>
-      </a>
+    </a>
 
-      </div>
-
-      <div class="navbar3">
-       <a href="/#" target="_blank" title="school">
-      <img src="/school.svg" class="" alt="">
+    <a href="/#" target="_blank" title="school">
+      <img src="/school.svg" class="" alt="" />
       <span>Escuela/ Institucion</span>
-      </a>
+    </a>
 
-       <a href="/#" target="_blank" title="nosotros">
-      <img src="/nosotros.svg" class="" alt="">
+    <a href="/#" target="_blank" title="nosotros">
+      <img src="/nosotros.svg" class="" alt="" />
       <span>Sobre nosotros</span>
-      </a>
+    </a>
 
-       <a href="/#" target="_blank" title="ayuda">
-      <img src="/ayuda.svg" class="" alt="">
+    <a href="/#" target="_blank" title="ayuda">
+      <img src="/ayuda.svg" class="" alt="" />
       <span>Ayuda</span>
-      </a>
-      </div>
-    
-    </li>
-  </ul>
+    </a>
+  </div>
 
- 
+  <div class="navbar2">
+    <a href="/#" target="_blank" title="min">
+      <img src="/min.svg" class="" alt="" />
+      <span></span>
+    </a>
+
+    <a href="/#" target="_blank" title="winmin">
+      <img src="/winmin.svg" class="" alt="" />
+      <span></span>
+    </a>
+    <a href="/#" target="_blank" title="close">
+      <img src="/cerrar.svg" class="" alt="" />
+      <span></span>
+    </a>
+  </div>
 </nav>
 
 <style>
-
- 
- nav {
+  nav {
     background-color: #094067;
-    flex-direction: row;
-  }
-
-  .logo{
-    border-radius: 50%;
-  }
-
-  nav ul {
     display: flex;
-    list-style: none;
-    margin: 0;
-    padding: 0;
-    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
   }
 
- 
+  .logo img {
+    height: 35px;
+    width: 45px;
+    border-radius: 50%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    display: inline;
+  }
 
   a {
     font-size: 10px;
     color: aliceblue;
-    padding-left: 0.5rem;
     padding: 0.2rem 0.2rem;
-    display:inline-flex;
-    flex-direction:column; /* Apila los elementos verticalmente */
-    align-items:center; /* Centra los elementos horizontalmente */
-
-
+    display: inline-flex;
+    flex-direction: column;
+    align-items: center;
   }
 
+  .navbar1 {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    display: inline;
+  }
 
- 
-
+  .navbar2 {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    display: inline;
+    align-items: center;
+    justify-content: space-between;
+  }
 </style>
