8d93c7ff06d7cd18a715fa7f4d865874a1639b78
moye30 <115683322+moye30@users.noreply.github.com>
2024-05-01 22:44:34 -0600

    primera vista de formulario(grupos)

--- interface/App.svelte
+++ interface/App.svelte
@@ -1,13 +1,10 @@
 <script lang="ts">
-  import Greet from './lib/Greet.svelte'
-  import Navbar from './app/Navbar.svelte'
-  import LoginForm from './app/LoginForm.svelte';
+  import Greet from "./lib/Greet.svelte";
+  import Navbar from "./app/Navbar.svelte";
+  import LoginForm from "./app/LoginForm.svelte";
+  import FormGrupos from "./app/FormGrupos.svelte";
 </script>
 
 <main>
   <Navbar />
 </main>
-
- <form>
-   <LoginForm />
- </form>
--- interface/app/Navbar.svelte
+++ interface/app/Navbar.svelte
@@ -1,101 +1,111 @@
 <script>
-  //  import newFileIcon from '../newFile.svg'
+  const Menus = {
+    principal: "principal",
+
+    grupos: "grupos",
+  };
+
+  import FormGrupos from "./FormGrupos.svelte";
+  let selectecMenu = Menus.principal;
 </script>
 
-<nav>
-  <div class="navbar1">
-    <a href="/login.svelte" target="_blank" title="Nuevo archivo">
-      <img src="/new_file.svg" class="" alt="" />
-      <span>Nuevo archivo</span>
-    </a>
+<div>
+  <nav>
+    <div class="navbar1">
+      <button
+        on:click={() => {
+          selectecMenu = Menus.principal;
+        }}
+      >
+        <img src="/new_file.svg" class="" alt="" />
+        <span>Nuevo archivo</span>
+      </button>
 
-    <a href="/#" target="_blank" title="Nueva carpeta">
-      <img src="/new_folder.svg" class="" alt="" />
-      <span>Nueva carpeta</span>
-    </a>
+      <a href="/#" target="_blank" title="Nueva carpeta">
+        <img src="/new_folder.svg" class="" alt="" />
+        <span>Nueva carpeta</span>
+      </a>
 
-    <a href="/#" target="_blank" title="Guardar">
-      <img src="/save.svg" class="" alt="" />
-      <span>Guadar</span>
-    </a>
+      <a href="/#" target="_blank" title="Guardar">
+        <img src="/save.svg" class="" alt="" />
+        <span>Guadar</span>
+      </a>
 
-    <a href="/#" target="_blank" title="preview">
-      <img src="/preview.svg" class="" alt="" />
-      <span>Vista previa</span>
-    </a>
+      <a href="/#" target="_blank" title="preview">
+        <img src="/preview.svg" class="" alt="" />
+        <span>Vista previa</span>
+      </a>
 
-    <a class="" href="/#" target="_blank" title="printer">
-      <img src="/printer.svg" class="" alt="" />
-      <span>Imprimir</span>
-    </a>
+      <a class="" href="/#" target="_blank" title="printer">
+        <img src="/printer.svg" class="" alt="" />
+        <span>Imprimir</span>
+      </a>
 
-    <a href="/#" target="_blank" title="group">
-      <img src="/group.svg" class="" alt="" />
-      <span>Grupos</span>
-    </a>
+      <button
+        on:click={() => {
+          selectecMenu = Menus.grupos;
+        }}
+      >
+        <img src="/group.svg" class="" alt="" />
+        <span>Grupos</span>
+      </button>
 
-    <a href="/#" target="_blank" title="materias">
-      <img src="/materias.svg" class="" alt="" />
-      <span>Materias</span>
-    </a>
+      <a href="/#" target="_blank" title="materias">
+        <img src="/materias.svg" class="" alt="" />
+        <span>Materias</span>
+      </a>
 
-    <a href="/#" target="_blank" title="maestros">
-      <img src="/maestro.svg" class="" alt="" />
-      <span>Maestros</span>
-    </a>
+      <a href="/#" target="_blank" title="maestros">
+        <img src="/maestro.svg" class="" alt="" />
+        <span>Maestros</span>
+      </a>
 
-    <a href="/#" target="_blank" title="aulas">
-      <img src="/aulas.svg" class="" alt="" />
-      <span>Aulas</span>
-    </a>
+      <a href="/#" target="_blank" title="aulas">
+        <img src="/aulas.svg" class="" alt="" />
+        <span>Aulas</span>
+      </a>
 
-    <a href="/#" target="_blank" title="IA">
-      <img src="/ia.svg" class="" alt="" />
-      <span>IA</span>
-    </a>
+      <a href="/#" target="_blank" title="IA">
+        <img src="/ia.svg" class="" alt="" />
+        <span>IA</span>
+      </a>
 
-    <a href="/#" target="_blank" title="hv">
-      <img src="/hv.svg" class="" alt="" />
-      <span>Historial de versiones</span>
-    </a>
+      <a href="/#" target="_blank" title="hv">
+        <img src="/hv.svg" class="" alt="" />
+        <span>H.Versiones</span>
+      </a>
 
-    <a href="/#" target="_blank" title="generador">
-      <img src="/generador.svg" class="" alt="" />
-      <span>Generar horario</span>
-    </a>
+      <a href="/generate_form" target="_blank" title="generador">
+        <img src="/generador.svg" class="" alt="" />
+        <span>Generar horario</span>
+      </a>
 
-    <a href="/#" target="_blank" title="school">
-      <img src="/school.svg" class="" alt="" />
-      <span>Escuela/ Institucion</span>
-    </a>
+      <a href="/#" target="_blank" title="school">
+        <img src="/school.svg" class="" alt="" />
+        <span>Institucion</span>
+      </a>
 
-    <a href="/#" target="_blank" title="nosotros">
-      <img src="/nosotros.svg" class="" alt="" />
-      <span>Sobre nosotros</span>
-    </a>
+      <a href="/#" target="_blank" title="nosotros">
+        <img src="/nosotros.svg" class="" alt="" />
+        <span>Sobre nosotros</span>
+      </a>
 
-    <a href="/#" target="_blank" title="ayuda">
-      <img src="/ayuda.svg" class="" alt="" />
-      <span>Ayuda</span>
-    </a>
+      <a href="/#" target="_blank" title="ayuda">
+        <img src="/ayuda.svg" class="" alt="" />
+        <span>Ayuda</span>
+      </a>
+    </div>
+  </nav>
+  <div style="width: 100%;">
+    {#if selectecMenu === Menus.principal}
+      <p>Condition is true</p>
+    {:else if selectecMenu === Menus.grupos}
+      <FormGrupos />
+    {:else}
+      <p>No true Condition is true</p>
+    {/if}
   </div>
-
-  <div class="navbar2">
-    <a href="/#" target="_blank" title="min">
-      <img src="/min.svg" class="" alt="" />
-      <span></span>
-    </a>
-
-    <a href="/#" target="_blank" title="winmin">
-      <img src="/winmin.svg" class="" alt="" />
-      <span></span>
-    </a>
-    <a href="/#" target="_blank" title="close">
-      <img src="/cerrar.svg" class="" alt="" />
-      <span></span>
-    </a>
-  </div>
-</nav>
+</div>
 
 <style>
   nav {
@@ -103,6 +113,7 @@
     display: flex;
     justify-content: space-between;
     align-items: center;
+    flex-direction: row;
   }
 
   .logo img {
