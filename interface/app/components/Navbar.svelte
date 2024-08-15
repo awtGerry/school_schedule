a083fec37caad4fd176107fa7dc7f6043ae02bf6
Victor Rodriguez <awtGerry@gmail.com>
2024-08-15 15:20:29 -0600

    nc: Trabajar con tauri windows
    nc = not complete

--- interface/app/components/Navbar.svelte
+++ interface/app/components/Navbar.svelte
@@ -1,6 +1,25 @@
 <script lang="ts">
   import { MenuData } from "../data/MenuData";
 
+  import { WebviewWindow } from "@tauri-apps/api/window";
+
+  import VistaMaterias from "../forms/VistaMaterias.svelte";
+
+  const win = new WebviewWindow("ss_window", {
+    url: "https://www.google.com",
+    title: "School Schedule",
+    width: 1000,
+    height: 800,
+    resizable: true,
+    maximizable: true,
+    transparent: false,
+    fullscreen: false,
+    alwaysOnTop: false,
+    visible: true,
+    x: 0,
+    y: 0,
+  });
+
   let isCollapsed = false;
   function colapseSidebar() {
     isCollapsed = !isCollapsed;
@@ -13,72 +32,79 @@
   }
 </script>
 
-<nav class="antialiased">
-  <div class="sidebar" class:collapsed={isCollapsed}>
+<div>
+  <nav class="antialiased">
+    <div class="sidebar" class:collapsed={isCollapsed}>
 
-    <div class="logo">
-      <img src="/logicirculo.svg" alt="Logo" />
-      {#if !isCollapsed}
-        <h1>School Schedule</h1>
-      {/if}
-    </div>
-
-    {#each MenuData as item}
-      <!-- Conditional rendering -->
-      {#if (item.it === 1 || item.it === 3)}
-        <span class="menu">{item.it === 1 ? "Menu" : "Utilidad"}</span>
-      {/if}
-      {#if (item.it === 2 || item.it === 4 || item.it === 1 || item.it === 3)}
-        <div class="lg:mt-4"></div>
-      {/if}
-      <div class="menu-item">
-        <button
-          class="btn"
-          data-menu={item.menu}
-          data-icon={item.icon}
-          data-name={item.name}
-          on:click={() => changeMenu(item.menu)}
-        >
-          <img src={item.icon} alt={item.name} />
-          {#if !isCollapsed}
-            <span>{item.name}</span>
-          {/if}
-          {#if item.submenu.length > 0}
-            <span class="arrow-container absolute right-0 mr-2 w-2 h-2">
-              <img src="/bar_icons/right-arrow.svg" alt="Arrow" />
-            </span>
-          {/if}
-        </button>
-
-        {#if isCollapsed && item.submenu.length == 0}
-          <div class="tooltip">{item.name}</div>
+      <div class="logo">
+        <img src="/logicirculo.svg" alt="Logo" />
+        {#if !isCollapsed}
+          <h1>School Schedule</h1>
         {/if}
-
-        {#if item.submenu.length > 0}
-          <ul class="submenu"> 
-            {#each item.submenu as subitem}
-              <li class="flex flex-row space-x-2">
-                <img class="w-4 h-4" src={subitem.icon} alt={subitem.name} />
-                <span>{subitem.name}</span>
-              </li>
-            {/each}
-          </ul>
-        {/if}
-
       </div>
-    {/each}
 
-    <!-- Toggle Button -->
-    <button class="toggle-btn" on:click={colapseSidebar}>
-      <img
-        class="w-6 h-6"
-        src={isCollapsed ? "/bar_icons/caret-right.svg" : "/bar_icons/caret-left.svg"}
-        alt="Toggle Sidebar"
-      />
-    </button>
+      {#each MenuData as item}
+        <!-- Conditional rendering -->
+        {#if (item.it === 1 || item.it === 3)}
+          <span class="menu">{item.it === 1 ? "Menu" : "Utilidad"}</span>
+        {/if}
+        {#if (item.it === 2 || item.it === 4 || item.it === 1 || item.it === 3)}
+          <div class="lg:mt-4"></div>
+        {/if}
+        <div class="menu-item">
+          <button
+            class="btn"
+            data-menu={item.menu}
+            data-icon={item.icon}
+            data-name={item.name}
+            on:click={() => changeMenu(item.menu)}
+          >
+            <img src={item.icon} alt={item.name} />
+            {#if !isCollapsed}
+              <span>{item.name}</span>
+            {/if}
+            {#if item.submenu.length > 0}
+              <span class="arrow-container absolute right-0 mr-2 w-2 h-2">
+                <img src="/bar_icons/right-arrow.svg" alt="Arrow" />
+              </span>
+            {/if}
+          </button>
 
-  </div>
-</nav>
+          {#if isCollapsed && item.submenu.length == 0}
+            <div class="tooltip">{item.name}</div>
+          {/if}
+
+          {#if item.submenu.length > 0}
+            <ul class="submenu"> 
+              {#each item.submenu as subitem}
+                <li class="flex flex-row space-x-2">
+                  <img class="w-4 h-4" src={subitem.icon} alt={subitem.name} />
+                  <span>{subitem.name}</span>
+                </li>
+              {/each}
+            </ul>
+          {/if}
+
+        </div>
+      {/each}
+
+      <!-- Toggle Button -->
+      <button class="toggle-btn" on:click={colapseSidebar}>
+        <img
+          class="w-6 h-6"
+          src={isCollapsed ? "/bar_icons/caret-right.svg" : "/bar_icons/caret-left.svg"}
+          alt="Toggle Sidebar"
+        />
+      </button>
+
+    </div>
+  </nav>
+  {#if selectedMenu !== "principal"}
+    <div class="materias-window">
+      <VistaMaterias />
+    </div>
+  {/if}
+</div>
 
 <style>
   nav {
@@ -291,5 +317,15 @@
     align-items: center; /* Centers the arrow vertically */
     justify-content: center; /* Centers the arrow horizontally */
     height: 100%; /* Ensure the container spans the full height of the button */
+  }
+
+  .materias-window {
+    position: fixed;
+    top: 0;
+    right: 0;
+    width: calc(100% - 240px);
+    height: 100vh;
+    padding: 1rem;
+    transition: width 0.3s ease;
   }
 </style>
